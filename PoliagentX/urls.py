e90b898200839c2b952747acc260274b7f242126
SCT211-0427/2023 GEORGE OTIENO <148488211+NGIYEG@users.noreply.github.com>
2025-08-12 15:47:03 +0300

    change

--- PoliagentX/urls.py
+++ PoliagentX/urls.py
@@ -7,13 +7,10 @@ from .views import*
 urlpatterns = [
  
     path("__reload__/", include("django_browser_reload.urls")),
-
     path('upload-indicators/', upload_indicators, name='upload_indicators'),
     path('', lambda request: redirect('upload_indicators', permanent=False)),
     path('download_indicator_template/', download_indicator_template, name='download_indicator_template'),
     path('download_budget_template/', download_budget_template, name='download_budget_template'),
-    # path('process_whole_budget/', process_whole_budget, name='process_whole_budget'),
-    # path('upload-expenditure/', upload_expenditure, name='upload_expenditure'),
     path('', lambda request: redirect('upload_expenditure', permanent=False)),
     path('upload_network/', upload_network, name='upload_network'),
     path('download_network_template/', download_network_template, name='download_network_template'),
@@ -21,4 +18,5 @@ urlpatterns = [
     path('calibration/',calibration,name='calibration'),
     path('simulation/', simulation, name='simulation'),
     path('start_calibration/', start_calibration, name='start_calibration'),
+    path('run_simulation/', run_simulation, name='run_simulation'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
